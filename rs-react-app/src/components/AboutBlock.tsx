97c6a5511cd338eb6297ad19660c418fb2d120ea
Nastya Belova <nastjabelova0@gmail.com>
2026-05-20 16:34:26 +0300

    feat: replace the text with required one in About page

--- rs-react-app/src/components/AboutBlock.tsx
+++ rs-react-app/src/components/AboutBlock.tsx
@@ -5,35 +5,16 @@ export const AboutBlock = (): JSX.Element => {
     <div className="block about-block">
       <h2 className="about-title">About:</h2>
       <div>
-        <blockquote>
-          <em>
-            Зажечь огни Земли, разжечь их по сторонам, <br />
-            Пока наш огонь горит, не страшно идти туда,
-            <br />
-            Где мраморны облака, где в грудь проникает смех,
-            <br />
-            И на вопрос «Как дела?», отвечу, что лучше всех.
-            <br />
-            Зажечь огни Земли, зажечь их по городам,
-            <br />
-            Ударит Царство Теней — я обернусь фениксом.
-            <br />
-            Ликуй, царица ночь, пускай страсть по сердцам,
-            <br />
-            И если уйду во тьму, что делать — я знаю сам, всё знаю сам!
-            <br />
-          </em>
-        </blockquote>
-        &copy;
-        <cite>
-          {' '}
-          <a href="https://www.youtube.com/watch?v=eV7Spwn37Vc">
-            -Bearwolf- (Phoenix)
-          </a>
-        </cite>{' '}
+        <em>
+          Nastya. <br />
+          Human, frontend developer.
+        </em>
         <br />
         <br />
-        <a href="https://rs.school/courses/reactjs">RSS React</a>
+        Student of{' '}
+        <a href="https://rs.school/courses/reactjs">
+          The Rolling Scopes School React Course.
+        </a>
       </div>
     </div>
   );
