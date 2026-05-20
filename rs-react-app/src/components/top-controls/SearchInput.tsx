12f6133db50d31886b8c5b7dd2ce26c83a44d311
Nastya Belova <nastjabelova0@gmail.com>
2026-05-20 16:49:46 +0300

    refactor: add map to datalist in SearchInput

--- rs-react-app/src/components/top-controls/SearchInput.tsx
+++ rs-react-app/src/components/top-controls/SearchInput.tsx
@@ -1,4 +1,5 @@
 import { useState, type ChangeEvent, type JSX } from 'react';
+import { ITEMS } from '../../constants';
 
 type SearchInputProps = {
   id: string;
@@ -36,46 +37,9 @@ export const SearchInput = (props: SearchInputProps): JSX.Element => {
         onFocus={handleInputOnfocus}
       ></input>
       <datalist id="suggestions">
-        <option value="animals" />
-        <option value="astronomical objects" />
-        <option value="book collections" />
-        <option value="books" />
-        <option value="book series" />
-        <option value="comics" />
-        <option value="conflicts" />
-        <option value="characters" />
-        <option value="comic collections" />
-        <option value="companies" />
-        <option value="comic series" />
-        <option value="comic strips" />
-        <option value="elements" />
-        <option value="episodes" />
-        <option value="foods" />
-        <option value="literature pieces" />
-        <option value="locations" />
-        <option value="magazines" />
-        <option value="medical conditions" />
-        <option value="movies" />
-        <option value="magazine series" />
-        <option value="materials" />
-        <option value="occupations" />
-        <option value="organizations" />
-        <option value="performers" />
-        <option value="seasons" />
-        <option value="spacecraft classes" />
-        <option value="series" />
-        <option value="soundtracks" />
-        <option value="species" />
-        <option value="spacecrafts" />
-        <option value="staff members" />
-        <option value="trading cards" />
-        <option value="technology pieces" />
-        <option value="titles" />
-        <option value="trading card decks" />
-        <option value="trading card sets" />
-        <option value="video games" />
-        <option value="video releases" />
-        <option value="weapons" />
+        {Object.values(ITEMS).map((item)=>(
+          <option key={item} value={item}></option>
+        ))}
       </datalist>
     </div>
   );
