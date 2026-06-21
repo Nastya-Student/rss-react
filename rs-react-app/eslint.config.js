3a3abb3ccf7e0c2a6611fd320ec71f46e0749a91
Nastya Belova <nastjabelova0@gmail.com>
2026-06-21 22:55:54 +0300

    feat: create the root layout

--- rs-react-app/eslint.config.js
+++ rs-react-app/eslint.config.js
@@ -15,7 +15,6 @@ export default defineConfig([
       js.configs.recommended,
       tseslint.configs.recommended,
       reactHooks.configs.flat.recommended,
-      reactRefresh.configs.vite,
       reactPlugin.configs.flat.recommended,
       reactPlugin.configs.flat['jsx-runtime'],
       eslintConfigPrettier,
---
+++ rs-react-app/src/app/layout.tsx
@@ -0,0 +1,19 @@
+import type { Metadata } from 'next';
+
+export const metadata: Metadata = {
+  title: 'rs-react-app',
+};
+
+export default function RootLayout({
+  children,
+}: {
+  children: React.ReactNode;
+}) {
+  return (
+    <html lang="en">
+      <body>
+        <div id="root">{children}</div>
+      </body>
+    </html>
+  );
+}
